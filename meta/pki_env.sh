14478d65c4590482729e57c029b16b8180eb475c
Patrick Sodré <sodre@sodre.co>
2017-12-11 08:05:32 +0000

    Fix PKI_ENABLED flag

--- meta/pki_env.sh
+++ meta/pki_env.sh
@@ -1,6 +1,8 @@
-export PKI_ENABLED="true"
 export PKI_HOME=${PARCELS_ROOT}/$PARCEL_DIRNAME
 
+PKI_ENABLED=false
 if [ -f pki-conf/init.sh ]; then
     . pki-conf/init.sh
+    PKI_ENABLED=true
 fi
+export PKI_ENABLED
